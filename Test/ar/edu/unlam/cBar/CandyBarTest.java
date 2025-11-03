139b6e08c633baee550750d19a90f6a67147c41e
mariano937-art <mardekyuss@gmail.com>
2025-11-03 15:40:45 -0300

    agregue mas test

--- Test/ar/edu/unlam/cBar/CandyBarTest.java
+++ Test/ar/edu/unlam/cBar/CandyBarTest.java
@@ -18,10 +18,9 @@ public class CandyBarTest {
 	private Snack palomitas;
 	private Bebida refresco;
 
-	
 	@Before
 	public void setUp() {
-		candyBar = new CandyBar(5); 
+		candyBar = new CandyBar(5); // Capacidad para 5 productos
 		palomitas = new Snack("Palomitas", 5.0, Tamano.MEDIANO, 10);
 		refresco = new Bebida("Refresco", 3.0, Contenedor.VASO, 15);
 	}
@@ -33,12 +32,12 @@ public class CandyBarTest {
 
 		assertTrue(candyBar.agregarProducto(refresco));
 		assertEquals(2, candyBar.contarProductosEnInventario());
+	}
 
-		try {
-			candyBar.agregarProducto(palomitas); // Intentamos agregar palomitas de nuevo
-			fail("Se esperaba una excepción ProductoDuplicadoException");
-		} catch (ProductoDuplicadoException e) {
-		}
+	@Test(expected = ProductoDuplicadoException.class)
+	public void testAgregarProductoDuplicado() throws ParametroInvalidoException, ProductoDuplicadoException {
+		candyBar.agregarProducto(palomitas); // Agregamos "Palomitas" al inventario
+		candyBar.agregarProducto(palomitas); // Intentamos agregar el mismo producto
 	}
 
 	@Test
@@ -55,4 +54,56 @@ public class CandyBarTest {
 			fail("Excepción inesperada: " + e.getMessage());
 		}
 	}
+
+	@Test(expected = ParametroInvalidoException.class)
+	public void testInventarioLleno() throws ParametroInvalidoException, ProductoDuplicadoException {
+		// Llenamos el inventario hasta su capacidad máxima (5 productos)
+		for (int i = 0; i < 5; i++) {
+			candyBar.agregarProducto(new Snack("Snack " + i, 1.0, Tamano.MEDIANO, 5));
+		}
+
+		// Intentamos agregar un sexto producto, lo cual debería lanzar una excepción
+		candyBar.agregarProducto(new Snack("Snack Extra", 1.0, Tamano.MEDIANO, 3));
+	}
+
+	@Test(expected = ParametroInvalidoException.class)
+	public void testAgregarProductoNull() throws ParametroInvalidoException, ProductoDuplicadoException {
+		candyBar.agregarProducto(null); 
+	}
+
+	
+
+	@Test(expected = ParametroInvalidoException.class)
+	public void testAgregarProductoExcesoCapacidad() throws ParametroInvalidoException, ProductoDuplicadoException {
+		for (int i = 0; i < 5; i++) {
+			candyBar.agregarProducto(new Snack("Snack " + i, 1.0, Tamano.MEDIANO, 5));
+		}
+
+		candyBar.agregarProducto(new Snack("Snack Extra", 1.0, Tamano.MEDIANO, 3));
+	}
+
+	@Test(expected = ParametroInvalidoException.class)
+	public void testEliminarProductoNull() throws ProductoNoEncontradoException, ParametroInvalidoException {
+		candyBar.eliminarProducto(null);
+	}
+
+	@Test(expected = ProductoNoEncontradoException.class)
+	public void testEliminarProductoNoExistente() throws ProductoNoEncontradoException, ParametroInvalidoException {
+		candyBar.eliminarProducto("ProductoInexistente");
+	}
+
+	@Test
+	public void testAgregarProductosHastaCapacidadMaxima() {
+		try {
+			for (int i = 0; i < 5; i++) {
+				candyBar.agregarProducto(new Snack("Snack " + i, 1.0, Tamano.MEDIANO, 5));
+			}
+
+			assertEquals(5, candyBar.contarProductosEnInventario());
+
+		} catch (ProductoDuplicadoException | ParametroInvalidoException e) {
+			fail("Excepción inesperada: " + e.getMessage());
+		}
+	}
+
 }
